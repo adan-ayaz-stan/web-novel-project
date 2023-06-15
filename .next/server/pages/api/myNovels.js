4f16a614dffa0c3f325cf2d78568ff569740fe5e
Adan <999a.d.a.n.a.y.a.z999@gmail.com>
2023-06-16 00:55:25 +0500

    Mass Release

--- .next/server/pages/api/myNovels.js
+++
@@ -1,1 +0,0 @@
-export const data = [{"novelId":"1359","title":"Supremacy Games","link":"https://readnovelfull.com/supremacy-games.html","img":"https://img.readnovelfull.com/thumb/t-300x439/supremacy-games-1636271846.jpg"}]
