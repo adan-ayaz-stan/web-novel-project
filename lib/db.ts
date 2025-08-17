2b0439dfcbf241ae31acb8de437d102792c4384c
Spitfire <adanayaztracer@gmail.com>
2025-08-17 17:35:47 +0500

    Add authToken to SQLite client configuration for secure access

--- lib/db.ts
+++ lib/db.ts
@@ -4,6 +4,7 @@ import { drizzle } from "drizzle-orm/libsql";
 // Create the client for SQLite database
 const client = createClient({
   url: "libsql://web-novel-project-spitfire.aws-eu-west-1.turso.io", // Local SQLite file
+  authToken: process.env.TURSO_DB_TOKEN,
 });
 
 export const db = drizzle(client);
